aca78d22a710e4dad46bce65f94bdb74c8c73d5f
kitik <svy1dan@gmail.com>
2026-04-21 21:56:36 +0300

    added new examples

--- functions/functions.go
+++ functions/functions.go
@@ -22,10 +22,29 @@ func add(a int, b int, sumFunction func(int, int) int) (sum int) { // <-- functi
 	return sumFunction(a, b)
 }
 
+func add2(a int, b int, sumFunciton func(int, int) int) int {
+	return sumFunciton(a, b)
+}
+
+func getFunction(functionName string) func(int, int) int {
+	switch functionName {
+	case "add":
+		return func(a int, b int) int {return a + b}
+	case "mult":
+		return func(a int, b int) int {return a * b}
+	}
+
+	return func(a int, b int) int {return a - b}
+
+}
+
 func main() {
 	fmt.Println(sum(1, 2))
 	fmt.Println(endlessVarsSum(1, 2, 3, 4, 5))
 	fmt.Println(add(1, 2, sum))
 	aninonimSumFunction := func(a int, b int) int { return a + b } // <-- anonim sum function
 	fmt.Println(aninonimSumFunction(1, 2))
+	fmt.Println(add2(1, 2, func(a int, b int) int { return a + b }))
+	someFunction := getFunction("mult")
+	fmt.Println(someFunction(5, 6)) // <-- function as function result
 }
