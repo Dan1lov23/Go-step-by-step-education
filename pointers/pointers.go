5fc210b79e7cfe42d7e685fb35b93af3cc0af7df
kitik <svy1dan@gmail.com>
2026-04-23 18:51:26 +0300

    add new theme

---
+++ pointers/pointers.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x int = 4 // <-- number variable
+	p := &x       // <-- pointer on x variable
+	fmt.Println("Num:", x, ", num pointer:", p)
+
+	*p = 12 // <-- chnage x variable value
+	fmt.Println("New number value:", x)
+
+	var strVarPointer *string  // <-- empty pointer
+	fmt.Println(strVarPointer) // <-- vil be nil
+
+	pointerN := new(int) // <-- make pointer bu using new command
+	*pointerN = 148
+	fmt.Println(pointerN)
+
+}
