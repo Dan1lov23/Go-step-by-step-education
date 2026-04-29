42b80dab89057e9db5064ceb575882fa2e54a3db
kitik <svy1dan@gmail.com>
2026-04-29 18:06:32 +0300

    add new examples for pointers theme

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func changeVarValue(x *int) {
+	*x = (*x) * (*x)
+}
+
+func createPointer(num int) *int { 
+
+	p := new(int)
+	*p = num
+
+	return p
+
+}
+
 func main() {
 
 	var x int = 4 // <-- number variable
@@ -25,4 +38,11 @@ func main() {
 	pointersArr[2] = &c
 	fmt.Println(pointersArr)
 
+	valueForChange := 5 // <-- before
+	fmt.Println(valueForChange)
+	changeVarValue(&valueForChange) // <-- function chnage variable value by using pointer
+	fmt.Println(valueForChange)     // <-- after
+
+	fmt.Println(createPointer(12)) // <-- pointer
+	fmt.Println(createPointer(18))
 }
